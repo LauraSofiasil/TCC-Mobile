ba962002a3ce05e67ec38dd81ab8d0d381893660
LAURA SOFIA LIMA NUNES DA SILVA <24122467@dsn127m1246730.cfp127aluno.local>
2025-10-07 17:26:06 -0300

    Construindo tela perfil

--- app/src/main/java/br/senai/sp/jandira/tcc/screens/TelaPerfil.kt
+++ app/src/main/java/br/senai/sp/jandira/tcc/screens/TelaPerfil.kt
@@ -10,14 +10,27 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -57,7 +70,134 @@ fun TelaPerfil(modifier: Modifier = Modifier) {
                     color = Color.White,
                     fontSize = 40.sp
                 )
-                Card() { }
+
+                Spacer(modifier = Modifier.height(40.dp))
+
+                Card(
+                    modifier = Modifier
+                        .width(300.dp)
+                        .height(499.dp),
+                    shape = RoundedCornerShape(
+                        topEnd = 10.dp,
+                        topStart = 10.dp
+                    ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color.White
+                    )
+                ) {
+                    Column() {
+                        Text(
+                            text = "Login",
+                            color = Color(0xff000000),
+                            style = MaterialTheme.typography.titleLarge,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Medium,
+                            modifier = Modifier
+                                .padding(start = 5.dp)
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        TextField(
+                            value = "",
+                            onValueChange = {},
+                            colors = TextFieldDefaults.colors(
+                                focusedTextColor = Color(0xff949494), //Cor do texto digitado - usuário clicou
+                                unfocusedTextColor = Color(0xff949494), //Cor do texto digitado - usuário não clicou
+                                cursorColor = Color.Black, //Cor do cursor enquanto digita
+                                focusedContainerColor = Color(0xffffffff), //Cor do fundo - usuário clicou
+                                unfocusedContainerColor = Color(0xffffffff), //Cor do fundo - usuário não clicou
+                                focusedIndicatorColor = Color.Transparent, //Cor da linha de baixo - usuário clicou
+                                unfocusedIndicatorColor = Color.Transparent //Cor da linha de baixo - usuário não clicou
+                            ),
+                            placeholder = {
+                                Text("Digite seu email...", color = Color(0xff949494))
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Person,
+                                    contentDescription = "",
+                                    tint = Color(0x52000000),
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                )
+                            },
+                            modifier = Modifier
+                                .width(300.dp),
+                            shape = RoundedCornerShape(12.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                    }
+
+                    Column() {
+                        Text(
+                            text = "Senha",
+                            color = Color(0xff000000),
+                            style = MaterialTheme.typography.titleLarge,
+                            fontSize = 15.sp,
+                            fontWeight = FontWeight.Medium,
+                            modifier = Modifier
+                                .padding(start = 5.dp)
+                        )
+
+                        Spacer(modifier = Modifier.height(8.dp))
+
+                        TextField(
+                            value = "",
+                            onValueChange = {},
+                            colors = TextFieldDefaults.colors(
+                                focusedTextColor = Color(0xff949494), //Cor do texto digitado - usuário clicou
+                                unfocusedTextColor = Color(0xff949494), //Cor do texto digitado - usuário não clicou
+                                cursorColor = Color.Black, //Cor do cursor enquanto digita
+                                focusedContainerColor = Color(0xffffffff), //Cor do fundo - usuário clicou
+                                unfocusedContainerColor = Color(0xffffffff), //Cor do fundo - usuário não clicou
+                                focusedIndicatorColor = Color(0xff0D5692), //Cor da linha de baixo - usuário clicou
+                                unfocusedIndicatorColor = Color(0xff0D5692) //Cor da linha de baixo - usuário não clicou
+                            ),
+                            placeholder = {
+                                Text("Digite sua senha...", color = Color(0xff949494))
+                            },
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Default.Lock,
+                                    contentDescription = "",
+                                    tint = Color(0x52000000),
+                                    modifier = Modifier
+                                        .size(20.dp)
+                                )
+                            },
+                            modifier = Modifier
+                                .width(300.dp),
+                            shape = RoundedCornerShape(12.dp),
+                            keyboardOptions = KeyboardOptions(
+                                keyboardType = KeyboardType.Number,
+                                imeAction = ImeAction.Next
+                            )
+                        )
+                    }
+
+                    Spacer(modifier = Modifier.height(50.dp))
+
+
+                }
+
+                Spacer(modifier = Modifier.height(20.dp))
+
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(65.dp),
+                    shape = RoundedCornerShape(
+                        topEnd = 10.dp,
+                        topStart = 10.dp
+                    ),
+                    colors = CardDefaults.cardColors(
+                        containerColor = Color(0xBA1892FF)
+                    )
+                ) { }
             }
         }
     }
