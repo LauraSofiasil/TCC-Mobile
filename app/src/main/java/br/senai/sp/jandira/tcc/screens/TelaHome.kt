f67697160eac548a859cd8c3c258a2595585bd42
LAURA SOFIA LIMA NUNES DA SILVA <24122467@CFP127ALUNO.LOCAL>
2025-10-14 17:22:48 -0300

    feat (Desenvolvimento Design Tela Home)

--- app/src/main/java/br/senai/sp/jandira/tcc/screens/TelaHome.kt
+++ app/src/main/java/br/senai/sp/jandira/tcc/screens/TelaHome.kt
@@ -1,12 +1,117 @@
 package br.senai.sp.jandira.tcc.screens
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import br.senai.sp.jandira.tcc.R
 
 @Composable
 fun TelaHome(navegacao: NavHostController?) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(Color(0xffDDE7F0))
+        ) {
+            //Barra Inferior
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(38.dp),
+                shape = RoundedCornerShape(
+                    bottomStart = 10.dp,
+                    bottomEnd = 10.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xBA1892FF)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 14.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {}
+            }
 
+            Image(
+                painter = painterResource(R.drawable.slidehome),
+                contentDescription = "",
+                modifier = Modifier
+                    .size(150.dp)
+            )
+
+            //Barra inferior
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(65.dp),
+                shape = RoundedCornerShape(
+                    topEnd = 10.dp,
+                    topStart = 10.dp
+                ),
+                colors = CardDefaults.cardColors(
+                    containerColor = Color(0xBA1892FF)
+                )
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(top = 14.dp),
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Image(
+                        painter = painterResource(R.drawable.calendario),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(80.dp))
+
+                    Image(
+                        painter = painterResource(R.drawable.casinha),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+
+                    Spacer(modifier = Modifier.width(80.dp))
+
+                    Image(
+                        painter = painterResource(R.drawable.medicina),
+                        contentDescription = "",
+                        modifier = Modifier
+                            .size(24.dp)
+                    )
+                }
+            }
+        }
+    }
 }
 
 @Preview
